5e91742aa139a4257c4013c38bdcfacef35016df
Nirupan Joe <nirupanjoej@gmail.com>
2021-07-24 08:26:55 +0000

    Add some style

--- src/components/screen.js
+++ src/components/screen.js
@@ -3,8 +3,8 @@ import context from '../core/context';
 import Input from './input';
 
 const Screen = () =>
-	<div>
-		<div>Score: {context.state.score}</div>
+	<div className="container">
+		<div className="score">Score: {context.state.score}</div>
 		<div>{context.state.question}</div>
 		<div>{Input()}</div>
 	</div>;
